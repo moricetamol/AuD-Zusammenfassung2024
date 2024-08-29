be08c772444b1bb90972873e053e852c89322ccf
= <gerhardt.mor@gmail.com>
2024-08-29 04:21:20 +0200

    Finished up section 8

--- Code/AdjacencyGraph.java
+++ Code/AdjacencyGraph.java
@@ -1,5 +1,11 @@
-class AdjacencyGraph{
-    private final AdjacencyRepresentation representation;
+import java.util.HashMap;
+import java.util.Map;
+
+class AdjacencyGraph<N>{
+    private final AdjacencyRepresentation representation; // adjlist or matrix
+    private final Map<N, Integer> nodeToIndex = new HashMap<>();
+    private final Map<Integer, N> indexToNode = new HashMap<>();
+
     public AdjacencyGraph(AdjacencyRepresentation representation) {
         this.representation = representation;
     }
